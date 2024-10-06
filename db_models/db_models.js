ace7b98c2ac2ad322b8d16e8b72fc30fe1ca2f23
Alex-nonc <degra1290@gmail.com>
2024-10-07 01:21:15 +0300

    -

--- db_models/db_models.js
+++ db_models/db_models.js
@@ -3,35 +3,36 @@ const {DataTypes} = require('sequelize')
 
 const Contact_data = sequelize.define ('Contact_data', {
     contact_data_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    contact_data_name: {type: DataTypes.STRING},
-    contact_data_email: {type: DataTypes.STRING, unique: true},
-    contact_data_phone: {type: DataTypes.STRING}
+    contact_data_name: {type: DataTypes.STRING, allowNull: false},
+    contact_data_email: {type: DataTypes.STRING, unique: true, allowNull: false},
+    contact_data_phone: {type: DataTypes.STRING, allowNull: false}
 })
 
 const Booking = sequelize.define ('Booking', {
     booking_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    booking_quantity: {type: DataTypes.INTEGER},
-    booking_date: {type: DataTypes.DATE}
+    booking_quantity: {type: DataTypes.INTEGER, allowNull: false},
+    booking_date: {type: DataTypes.DATE, allowNull: false}
 })
 
 const Type_client = sequelize.define ('Type_client', {
     type_client_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    type_client: {type: DataTypes.STRING},
-    type_client_price: {type: DataTypes.INTEGER},
+    type_client: {type: DataTypes.STRING, allowNull: false},
+    type_client_price: {type: DataTypes.INTEGER, allowNull: false},
 })
 
 const Sign_up = sequelize.define ('Sign_up', {
     sign_up_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    sign_up_phone: {type: DataTypes.STRING},
-    sign_up_email: {type: DataTypes.STRING, unique: true},
-    sign_up_passwd: {type: DataTypes.STRING},
-    sign_up_name: {type: DataTypes.STRING}
+    sign_up_phone: {type: DataTypes.STRING, allowNull: false},
+    sign_up_email: {type: DataTypes.STRING, unique: true, allowNull: false},
+    sign_up_passwd: {type: DataTypes.STRING, allowNull: false},
+    sign_up_name: {type: DataTypes.STRING, allowNull: false},
+    sign_up_role: {type: DataTypes.STRING, allowNull: false},
 })
 
 const Comments = sequelize.define ('Comments', {
     comments_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    comments_date: {type: DataTypes.DATE},
-    comments_comment: {type: DataTypes.STRING}
+    comments_date: {type: DataTypes.DATE, allowNull: false},
+    comments_comment: {type: DataTypes.STRING, allowNull: false}
 })
 
 Contact_data.hasOne(Booking)
